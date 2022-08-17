85acf4b1efa646e291f7715a006cefaf801f7b3b
Kyle Morton <kylemortondevelopment@gmail.com>
2022-08-16 22:20:47 -0500

    init source

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Kyle Morton on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
