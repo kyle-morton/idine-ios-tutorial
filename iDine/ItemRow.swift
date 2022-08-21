1011e6219f9ce02c4968a0e557f15c14766f3dd1
Kyle Morton <kylemortondevelopment@gmail.com>
2022-08-20 22:50:34 -0500

    Updating asset references, using these in itemView

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -11,7 +11,14 @@ struct ItemRow: View {
     let item: MenuItem;
     
     var body: some View {
-        Text(item.name);
+        HStack {
+            Image(item.thumbnailImage)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text(String("$\(item.price)"))
+            }
+        }
+
     }
 }
 
--- iDine/Models/Menu.swift
+++ iDine/Models/Menu.swift
@@ -25,10 +25,10 @@ struct MenuItem: Codable, Equatable, Identifiable {
     var mainImage: String {
         name.replacingOccurrences(of: " ", with: "-").lowercased()
     }
-
-//    var thumbnailImage: String {
-//        "\(mainImage)-thumb"
-//    }
+    
+    var thumbnailImage: String {
+        "\(name.replacingOccurrences(of: " ", with: "-").lowercased())-thumb"
+    }
 
     #if DEBUG
     static let example = MenuItem(id: UUID(), name: "Maple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you…")
