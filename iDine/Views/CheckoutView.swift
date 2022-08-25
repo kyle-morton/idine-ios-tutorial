a93fa6d781e93b6c913670f734b713764299f59f
Kyle Morton <kylemortondevelopment@gmail.com>
2022-08-24 22:13:53 -0500

    adding functionality for the checkout view

--- iDine/Views/CheckoutView.swift
+++ iDine/Views/CheckoutView.swift
@@ -11,8 +11,24 @@ struct CheckoutView: View {
     
     @EnvironmentObject var order: Order
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    let tipAmounts = [10, 15, 20, 25, 0]
     
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
+    
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        
+        let total = Double(order.total)
+        let tipValue = total/100 * Double(tipAmount)
+        
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
+    }
+
     
     var body: some View {
         Form {
@@ -21,6 +37,26 @@ struct CheckoutView: View {
                     Text($0)
                 }
             }
+            Toggle("Add iDine Loyalty Card", isOn: $addLoyaltyDetails.animation())
+            if addLoyaltyDetails {
+                TextField("Enter your iDine ID", text: $loyaltyNumber)
+            }
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section(header: Text("TOTAL: \(totalPrice)")) {
+                Button("Confirm Order") {
+                    showingPaymentAlert.toggle()
+                }
+            }
+        }
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was \(totalPrice) – thank you!"), dismissButton: .default(Text("OK")))
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
